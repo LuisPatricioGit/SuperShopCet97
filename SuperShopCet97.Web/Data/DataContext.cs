b705b083d8144bb66637843df5113dd5dbb1b39b
LuisPatricioGit <luispatricio.info@gmail.com>
2025-04-24 17:17:58 +0100

    First Controller

---
+++ SuperShopCet97.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShopCet97.Web.Data.Entities;
+
+namespace SuperShopCet97.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
