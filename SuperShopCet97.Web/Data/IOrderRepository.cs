f43b80cabe65e9d6d2e93e10ca88e25bf3e88613
LuisPatricioGit <luispatricio.info@gmail.com>
2025-05-15 17:52:45 +0100

    Increase, Decrease and Delete Detail Temp Item

--- SuperShopCet97.Web/Data/IOrderRepository.cs
+++ SuperShopCet97.Web/Data/IOrderRepository.cs
@@ -14,5 +14,9 @@ namespace SuperShopCet97.Web.Data
         Task AddItemToOrderAsync(AddItemViewModel model, string userName);
 
         Task ModifyOrderDetailTempQuantityAsync(int id, double quantity);
+
+        Task DeleteDetailTempAsync(int id);
+
+
     }
 }
